29d211c09e62a2baf29ddacc9788b29bf80b805b
momo <momo@aiskorea.co.kr>
2021-02-22 22:55:53 +0900

    bug: Kakao api update error해결하기
      implementation "com.kakao.sdk:v2-user:2.3.0"를
    implementation group: project.KAKAO_SDK_GROUP, name: 'usermgmt',
     version: project.KAKAO_SDK_VERSION로 교체하기

--- front/Screens/Apply/ApplyHome.js
+++ front/Screens/Apply/ApplyHome.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
-import { View, Text, ScrollView, StyleSheet, SafeAreaView } from "react-native";
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
 
-import WorkCalendar from "../../Shared/WorkCalendar";
-import ApplyModal from "../../Shared/ApplyModal";
+import WorkCalendar from '../../Shared/WorkCalendar';
+import ApplyModal from '../../Shared/ApplyModal';
 
 const ApplyHome = ({ navigation, route }) => {
-  const [nickname, setNickname] = useState("momo");
+  const [nickname, setNickname] = useState('momo');
   const [annualLeave, setAnnualLeave] = useState(12);
 
   return (
@@ -13,7 +20,7 @@ const ApplyHome = ({ navigation, route }) => {
       <ScrollView style={styles.scrollView}>
         <View>
           <Text style={styles.viewText}>
-            현재 {nickname}님이 사용할 수 있는{"\n"}
+            현재 {nickname}님이 사용할 수 있는{'\n'}
             연차일은 <Text style={{ fontSize: 30 }}>{annualLeave}</Text>일
             입니다.
           </Text>
@@ -29,18 +36,18 @@ const ApplyHome = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   scrollView: {
     marginTop: 20,
   },
   iosOnly: {
-    paddingTop: Platform.OS === "ios" && 36,
+    paddingTop: Platform.OS === 'ios' ? 36 : 0,
   },
   viewText: {
-    fontSize: Platform.OS === "ios" ? 20 : 15,
-    textAlign: "center",
+    fontSize: Platform.OS === 'ios' ? 20 : 15,
+    textAlign: 'center',
     marginTop: 10,
   },
 });
